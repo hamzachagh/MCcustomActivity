c78baaf2bb8ffa68d63c7ec607c9cd226f83168b
Hamza Chagh <hamza.chagh@salesforce.com>
2018-05-13 13:34:34 +0000

    app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+var app = express();
+
+app.get('/salesforceRest', (req , res) => {
+  
+    res.send('hellooooo hamzlla chagh !!');
+    
+  });
+console.log("hamza chagh")
